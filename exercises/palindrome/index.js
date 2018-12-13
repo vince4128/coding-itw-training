6c6d25683f69d566f8d4b072fea5d45596c121b5
vince4128 <vrlt.vincent@gmail.com>
2018-12-13 17:29:34 +0100

    first 2 exercises solved

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+//solution vincent
+/*function palindrome(str) {
+   const reverse = str.split('').reduce((rev, char) => {
+        return char + rev;
+    }, '');
+    return str === reverse;
+}*/
+
+//solution 2 stephen
+/*function palindrome(str){
+    const reversed = str.split('').reverse().join('');
+    return str === reversed;
+}*/
+
+//solution 3 stephen
+function palindrome(str){
+    //first parameter of every is the value and second is the index
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i -1 ]; //minus one to deal with the 0 base
+    });
+}
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//solution 1
+//https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/reverse
+
+/*function reverse(str) {
+    return str.split('').reverse().join('');
+}*/
+
+//solution 2
+/*function reverse(str){
+    let reversed = '';
+
+    for (let character of str){
+        reversed = character + reversed;
+    }
+
+    return reversed;
+}*/
+
+//solution 3
+function reverse(str){
+    return str.split('').reduce((rev, char) => {
+        return char + rev;
+    }, '');
+}
 
 module.exports = reverse;
